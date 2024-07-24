db93fe0ce4dbdf5e6ec265ad3f9143ec9ab7f403
NgaLe02 <ngalee2002@gmail.com>
2024-07-24 11:16:52 +0700

    update

--- src/main/java/com/docker/springboot/model/User.java
+++ src/main/java/com/docker/springboot/model/User.java
@@ -11,7 +11,9 @@ import java.io.Serializable;
 @AllArgsConstructor
 public class User implements Serializable {
 
-    private int userId;
+    private static final long serialVersionUID = 1L;
+    
+	private String userId;
 	private String name;
     private String phone;
     private String email;
