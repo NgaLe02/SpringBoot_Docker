c2158b1956661953a9f5e95a617e19d552f22b70
NgaLe02 <113868986+NgaLe02@users.noreply.github.com>
2024-07-24 09:55:22 +0700

    update

--- src/main/java/com/docker/springboot/model/User.java
+++ src/main/java/com/docker/springboot/model/User.java
@@ -5,33 +5,13 @@ import lombok.*;
 import java.io.Serializable;
 
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User implements Serializable {
 
-    private String userId;
-    public String getUserId() {
-		return userId;
-	}
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPhone() {
-		return phone;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    private int userId;
 	private String name;
     private String phone;
     private String email;
